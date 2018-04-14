5fce43f744dd0809fe3943a6441a971ab33ff2b8
hjdtl <873804682@qq.com>
2018-04-14 17:27:37 +0800

    loading style

---
+++ lab/lab.js
@@ -0,0 +1,206 @@
+let loading={
+    template:'<div id="loading_">\n' +
+    '    <div class="loaders">\n' +
+    '      <div class="loader">\n' +
+    '        <div class="loader-inner line-scale-pulse-out-rapid">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>\n' +
+    '    </div>\n' +
+    '  </div>'
+}
+
+let loading2={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-pulse">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading3={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-grid-pulse">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading4={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-clip-rotate">\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading5={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-clip-rotate-pulse">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading6={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner square-spin">\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading7={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-clip-rotate-multiple">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading8={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-pulse-rise">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading9={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-rotate">\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading10={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-triangle-path">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading11={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-scale">\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading12={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner line-scale">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading13={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner line-scale-party">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading14={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-scale-multiple">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading15={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-scale-ripple">\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading16={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-scale-ripple-multiple">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading17={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner ball-spin-fade-loader">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading18={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner line-spin-fade-loader">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
+
+let loading19={
+    template:'<div class="loader">\n' +
+    '        <div class="loader-inner pacman">\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '          <div></div>\n' +
+    '        </div>\n' +
+    '      </div>'
+}
